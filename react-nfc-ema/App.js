8f2c5695d2f2feab49c7401473414c5dfab54b62
manukito2012 <66195469+manukito2012@users.noreply.github.com>
2024-10-24 01:29:14 -0300

    Add files via upload

---
+++ react-nfc-ema/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import {View, Button, Text} from 'react-native'
+import NfcCardReader from 'react-native-nfc-card-reader'
+
+
+
+
+export default class App extends Component {
+
+
+  state = {
+    cardNumber : null,
+    expiryDate : null,
+    cardType : null,
+    firstname : null,
+    surname : null,
+ }
+
+ async startScan(){
+   let that = this;
+   await NfcCardReader.startNfc(function(cardDetails){
+     console.log(cardDetails)
+     that.setState({cardNumber : cardDetails.cardNumber})
+     that.setState({expiryDate : cardDetails.expiryDate})
+     that.setState({cardType : cardDetails.cardType})
+     that.setState({firstname : cardDetails.firstname})
+     that.setState({surname : cardDetails.surname})
+    })
+ }
+
+  render() {
+  return <View style={{flex:1,justifyContent: 'center',margin:20, alignContent:'center'}}>
+    <Button title="Start NFC Scan!" onPress={() => {this.startScan()}} ></Button>
+    <Text>{this.state.cardNumber}</Text>
+    <Text>{this.state.expiryDate}</Text>
+    <Text>{this.state.cardType}</Text>
+    <Text>{this.state.firstname}</Text>
+    <Text>{this.state.lastname}</Text>
+
+  </View>
+   
+  }
+}
---
+++ react-nfc-ema/README.md
@@ -0,0 +1,8 @@
+# react-native-nfc-card-reader-sample
+Sample app for the react-native-nfc-card-reader module
+
+## How to run it
+Simple download it, and ensure you run `npm install` once that's done it should be a case of running the Android app via React.
+
+In my case I use `npx react-native run-android` 
+
